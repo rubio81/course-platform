d65da2d550dbfcebb8e434a4da9245f5c14b6636
Jose Rubio <Jose.Rubio@fonua.com>
2020-11-21 12:30:43 +0000

    chore: add apps

---
+++ apps/course-client/src/app/lesson/lesson.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'course-platform-lesson',
+  templateUrl: './lesson.component.html',
+  styleUrls: ['./lesson.component.scss']
+})
+export class LessonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
