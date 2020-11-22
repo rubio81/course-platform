98826826badc503369025fb52f70f74d1b7dcbbf
Jose Rubio <Jose.Rubio@fonua.com>
2020-11-22 22:58:23 +0000

    test: changed message

--- apps/course-client-e2e/src/integration/app.spec.ts
+++ apps/course-client-e2e/src/integration/app.spec.ts
@@ -3,7 +3,7 @@ import { getGreeting } from '../support/app.po';
 describe('course-client', () => {
 	beforeEach(() => cy.visit('/course'));
 
-	it('should show lesson', () => {
+	it('should show lesson 1', () => {
 		cy.contains('1.- First lesson');
 	});
 });
