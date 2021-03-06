70f6ebe500cfbde9140a054768a09acd92edee4f
rubio81 <josemaria.rubio@gmail.com>
2021-03-06 20:59:26 +0000

    test: removed feature-toggling tests

--- libs/shared/util/util-feature-toggle/src/lib/shared/util-util-feature-toggle.spec.ts
+++
@@ -1,9 +0,0 @@
-import { sharedutilUtilFeatureToggle } from './shared/util-util-feature-toggle';
-
-describe('sharedutilUtilFeatureToggle', () => {
-	it('should work', () => {
-		expect(sharedutilUtilFeatureToggle()).toEqual(
-			'shared-util-util-feature-toggle',
-		);
-	});
-});
